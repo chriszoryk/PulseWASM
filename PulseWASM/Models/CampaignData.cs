c900cba78a14b95d6c7b504443746c83b55dbaa5
Christopher Zoryk <chrisz@thetin.net>
2024-10-09 16:02:23 +0100

    initial commit

---
+++ PulseWASM/Models/CampaignData.cs
@@ -0,0 +1,9 @@
+namespace PulseWASM.Models;
+
+public class CampaignData
+{
+    public string Title { get; set; }
+    public string Idea { get; set; }
+    public string IdeaTitle { get; set; }
+    public string SelectedPersona { get; set; }
+}
